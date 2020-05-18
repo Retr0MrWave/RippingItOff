a8bd49a20354ac7295566cff640905df3df7333b
Ilia <ilia.zhuravok@gmail.com>
2020-05-18 12:38:54 +0300

    Initial commit

---
+++ Assets/Scripts/HammerController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HammerController : MonoBehaviour
+{
+    private Rigidbody2D rb2D;
+    public float k;
+
+    void Start()
+    {
+        rb2D = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 screenPosition = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+        Vector2 worldPosition = Camera.main.ScreenToWorldPoint(screenPosition);
+        Vector2 force = worldPosition - rb2D.position;
+        rb2D.AddForce(force*k);
+    }
+}
